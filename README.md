3289b55b4ad342557e743f287c2dd0f594d21b60
dev_h.js <121519834+hyeon-js@users.noreply.github.com>
2023-09-02 15:19:17 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# dtro-info
-DaeguSubwayMap Back-end
+# 대구 도시철도 운행 정보 API
+© 2023 Hyeon.js, All rights reserved.
+
+- 시간표 기반 대구 도시철도 운행 정보 API
---
+++ index.js
@@ -0,0 +1,11 @@
+const dtro = require('./dtro.js');
+
+const http = require('http');
+http.createServer(async (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8;',
+        'Access-Control-Allow-Origin': '*'
+    });
+    // res.write(JSON.stringify(dtro(), null, 4));
+    res.end();
+}).listen(81);
